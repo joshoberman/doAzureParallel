c28a7bb01f39ada8a130c5581595fad418ed4cfc
Josh Oberman <joshoberman@gmail.com>
2017-08-04 18:19:52 -0500

    small debug to download function

--- R/utility.R
+++ R/utility.R
@@ -19,7 +19,24 @@ getURLInstallationCommand <- function(packages){
   if(length(packages) != 0){
     for(package in packages){
       installation <- paste0(installation,
-                             sprintf("Rscript -e \'args <- commandArgs(TRUE)\' -e \'devtools::install_url(args[1])\' %s %s", package),
+                             sprintf("Rscript -e \'args <- commandArgs(TRUE)\' -e \'devtools::install_url(args[1])\' %s", package),
+                             ";")
+    }
+  }
+  
+  installation <- substr(installation, 1, nchar(installation) - 1)
+}
+
+getVersionInstallationCommand <- function(packages, versions){
+  installation <- ""
+  installation <- paste0(installation,
+                         sprintf("Rscript -e \'args <- commandArgs(TRUE)\' -e \'install.packages(args[1], dependencies=TRUE)\' %s", "devtools"),
+                         ";")
+  
+  if(length(packages) != 0){
+    for(package in packages){
+      installation <- paste0(installation,
+                             sprintf("Rscript -e \'args <- commandArgs(TRUE)\' -e \'devtools::install_version(args[1])\' %s %s", package),
                              ";")
     }
   }
